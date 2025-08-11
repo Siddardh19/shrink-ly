4ee06831e88d50cd34fd2f7944bf93053337dd80
Siddardh19 <siddardhsu@gmail.com>
2025-08-12 00:14:03 +0530

    Fist commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const { connectToMongoDB}= require("./connect");
+const URL = require("./models/url");
+
+const urlRoute = require("./routes/url");
+
+const app=express();
+const PORT= 8001;
+
+connectToMongoDB("mongodb://localhost:27017/short-url").then(() => console.log('Mongodb connected'));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/url", urlRoute);
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
